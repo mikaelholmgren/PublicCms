1b50cb34d455c70fd77bfdadb797e9ceb7987b5c
Mikael Holmgren <mikael.holmgren@iths.se>
2021-06-08 10:46:23 +0200

    Setup Microservice solution for VisitorStatistics

---
+++ VisitorCounterAPI/Models/Visitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisitorCounterAPI.Models
+{
+    public class Visitor
+    {
+        public Guid Id { get; set; }
+        public int Visits { get; set; }
+    }
+}
