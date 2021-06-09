ba035962886a21d4ec264136b58b8ba700730e2c
Mikael Holmgren <mikael.holmgren@iths.se>
2021-06-09 16:30:56 +0200

    Added Admin settings page, global settings for the site

--- PublicCms.Web/Models/SiteSettings.cs
+++ PublicCms.Web/Models/SiteSettings.cs
@@ -1,6 +1,7 @@
 ﻿using PublicCms.Web.Models.PageParts;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,9 +13,12 @@ namespace PublicCms.Web.Models
         {
             Type = SettingsTypes.Site;
         }
+        [Display(Name ="Sajtens namn")]
         public string SiteName { get; set; }
         public string ThemeCssFile { get; set; } = "";
         public List<PageNavigationItem> TopNavigation { get; set; } = new();
         public List<BasePart> Footer { get; set; }
+        [Display(Name ="Text på cookiebanner")]
+        public string ConsentText { get; set; }
     }
 }
---
+++ PublicCms.Web/Pages/Cms/Settings.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PublicCms.Web.Services;
+
+namespace PublicCms.Web.Pages.Cms
+{
+    public class SettingsModel : PageModel
+    {
+        private readonly ISettingsService _ss;
+
+        public SettingsModel(ISettingsService ss)
+        {
+            this._ss = ss;
+        }
+        [BindProperty]
+        public string SiteName { get; set; }
+        [BindProperty]
+        public string ConsentText { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var settings = await _ss.GetSiteSettingsAsync();
+            SiteName = settings.SiteName;
+            ConsentText = settings.ConsentText;
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            var settings = await _ss.GetSiteSettingsAsync();
+            settings.SiteName = SiteName;
+            settings.ConsentText = ConsentText;
+            await _ss.SaveSiteSettingsAsync(settings);
+            return RedirectToPage("./settings");
+        }
+    }
+}
